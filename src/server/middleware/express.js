bc8dd361f1ca5c6c6fed9b404d66717d48fee763
Adrien Antoine <adriantoine@gmail.com>
2015-09-11 14:36:50 +0100

    Changed the way to load server

--- src/server/middleware/express.js
+++ src/server/middleware/express.js
@@ -3,12 +3,13 @@ import _ from 'lodash';
 import graphqlHTTP from 'express-graphql';
 import { setConfig } from '../../config';
 
-export default function (options) {
+
+export default function (app, options) {
 
   let graphQLOptions = _.extend({ schema: require('../schema/schema')}, options);
 
   setConfig(options);
 
-  return graphqlHTTP(graphQLOptions);
+  app.use('/graphql', graphqlHTTP(graphQLOptions));
 
 }
